540aa1c4be947da6e9eddca90637d8c385a17a39
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-03-01 23:42:55 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from './components/Layout/Header';
+
 function App() {
   return (
-    <div>
-      <h2>React Restaurant</h2>
-    </div>
+    <>
+      <Header></Header>
+    </>
   );
 }
 
