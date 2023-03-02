675cab9622e5784f658a656d03043e32c13e0325
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-03-02 12:20:07 +0100

    show number of  meals in cart using cart context

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  // number of meals != number of items
+  // an item could have one or multiple meals
+  const cartMealsCount = cartCtx.items.reduce((accumulator, item) => {
+    return (accumulator += item.amount);
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{cartMealsCount}</span>
     </button>
   );
 };
