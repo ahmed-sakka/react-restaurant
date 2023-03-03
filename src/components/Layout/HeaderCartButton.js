593145dfd90631595db1ad4d1e53be1c034bea08
Ahmed Sakka <ahmedsakka720@gmail.com>
2023-03-03 12:05:02 +0100

    animate cart button on cart change

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
   // number of meals != number of items
   // an item could have one or multiple meals
-  const cartMealsCount = cartCtx.items.reduce((accumulator, item) => {
+  const cartMealsCount = items.reduce((accumulator, item) => {
     return (accumulator += item.amount);
   }, 0);
+  // handle button animation; bump when the cart changes
+  const [btnIsBumping, setBtnIsBumping] = useState(false);
+  useEffect(() => {
+    // executes only if there's at least one item in the cart
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsBumping(true);
+    // remove button bump class after 300 milliseconds so the button
+    // will bump again if the cart changes
+    const timer = setTimeout(() => {
+      setBtnIsBumping(false);
+    }, 300);
+    // cleanup function
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+  const btnClasses = `${classes.button} ${btnIsBumping ? classes.bump : ''}`;
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
